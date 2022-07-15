ca6a19c757a92deaee9139cfc9cd46b12f0d88e3
MareMare <mare.fujii@gmail.com>
2022-07-15 17:37:39 +0900

    Commit solution structures.

---
+++ src/DummyFileCreator.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace DummyFileCreator.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // NOTE: [Powershellでランダムな文字列で埋め尽くされたダミーファイルを作る。 \| 迷惑堂本舗](https://maywork.net/computer/powershell-random-dummyfile/)
+        // NOTE: [【Windows】ダミーファイルを任意のサイズで作成する \| とあるオタクの徒然日記](https://tsuredurediary.com/archives/windows-dummy-file.html)
+        // NOTE: [RandomNumberGenerator\.GetNonZeroBytesメソッドでバイト配列を非ゼロの乱数で満たす \(C\#\) \- smdn\.jp](https://smdn.jp/programming/dotnet-samplecodes/mathematics/3a9ed42a024111eb907175842ffbe222/)
+        // NOTE: [RNGCryptoServiceProviderクラスを暗号乱数ジェネレータとして使用する \(C\#\) \- smdn\.jp](https://smdn.jp/programming/dotnet-samplecodes/mathematics/3a9f168f024111eb907175842ffbe222/)
+    }
+}
